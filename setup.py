c8e1095e634fd6766951b62a2584e6bf3a5e3d09
landiisotta <landi@fbk.eu>
2020-09-09 15:27:50 -0400

    new version release

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="reval",
-    version="0.0.2",
+    version="1.0.0",
     author="Isotta Landi",
     author_email="isotta.landi@iit.it",
     description="Relative clustering validation to select best number of clusters",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/IIT-LAND/reval_clustering",
-    download_url="https://github.com/IIT-LAND/reval_clustering/releases/tag/v1.1",
+    download_url="https://github.com/IIT-LAND/reval_clustering/releases/tag/v1.1.1",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
